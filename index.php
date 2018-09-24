e01122b15af71344c6a6642d4cccdf09b8e299f6
Gonzalo Sáez Martí <Gonzalo Sez Mart@192.168.145.131>
2018-09-24 19:50:36 +0200

    Comit del Lunes 24-9
    Con Validación del Formulario de inicio de sesión.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+-<!DOCTYPE html>
 
 <html>
     <head>
@@ -6,34 +6,55 @@
         <title>PRUEBA DE PHP CON BOOTSTRAP</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
     </head>
-    <body>
+    <body style="background-color: #666">
         <div class="container">
             <div class="row">
-                    <div class="col-4">
-                        <button id="boton1" class="btn btn-primary">Primary</button>
-                    </div>
-                    <div class="col-4">
-                        <input id="cajaNombre" name="usuario_nombre" type="text" placeholder="Usuario">
-                    </div>
-                    <div class="col-4"></div>
+                <div class="col-12">
+                    <h2 class="text-center" style="color:white;">EJEMPLO DE INICIO SESIÓN EN PHP</h2>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+
+                </div>
+                <div class="col-4">
+                    <br><br>
+                    <form action="index2.php">
+                        <input id="cajaNombre" class="form-control" type="text" placeholder="Usuario" required="required">
+                        <br>
+                        <input id="cajaPassword" class="form-control" type="text" placeholder="Contraseña" required="required">
+                        <br>
+                        <button id="boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
+                        <br>
+                        <p>
+                            <input class="form-control" data-validation="date" data-validation-format="yyyy-mm-dd" placeholder="Year (yyyy-mm-dd):">
+                        </p>
+                    </form>
+                </div>
+                <div class="col-4"></div>
+            </div>
         </div>
-        
+
         <?php
         // put your code here
         ?>
     </body>
     <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-    
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+
     <script>
         // document ready se ejecuta cuando toda la pagina se ha cargado completamente
-        $(document).ready(function(){
-            $('#cajaNombre').hide();
+        $(document).ready(function () {
+            //$('#cajaNombre').hide();
         });
-        
-        $('#boton1').click(function(){
+
+        $.validate({
+            lang: 'es'
+        });
+
+        $('#boton1').click(function () {
             console.log("aqui si que llega");
-            $('#cajaNombre').show(200);
+            $('#cajaNombre').show();
         })
     </script>
 </html>
