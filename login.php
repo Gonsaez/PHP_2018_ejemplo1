7707fedb51c1c059b994b89587677d96a557731e
Gonzalo Sáez Martí <Gonzalo Sez Mart@192.168.145.131>
2018-10-02 19:23:23 +0200

    A prueba de accesos no permitidos creando una function.

--- login.php
+++ login.php
@@ -1,9 +1,23 @@
 <?php
 include ('misFunciones.php');
+function limpiaPalabra($palabra){
+    $palabra = trim($palabra, "'");
+    $palabra = trim($palabra, " ");
+    $palabra = trim($palabra, "-");
+    $palabra = trim($palabra, "`");
+    $palabra = trim($palabra, '"');
+    return $palabra;
+}
+
 $mysqli = conectaBBDD();
  $cajaNombre = $_POST['cajaNombre'];
  
  $cajaPassword = $_POST['cajaPassword'];
+ 
+ //Filtro muy básico para evitar la inyección de SQL
+ $cajaNombre = limpiaPalabra($_POST['cajaNombre']);
+ $cajaPassword = limpiaPalabra($_POST['cajaPassword']);
+ 
 //echo 'Has escrito el usuario: '.$cajaNombre.' y la contraseña: '.$cajaPassword; 
  
  $resultadoQuery = $mysqli -> query("SELECT * FROM usuarios WHERE nombreUsuario = '$cajaNombre'AND userPass = '$cajaPassword'");
