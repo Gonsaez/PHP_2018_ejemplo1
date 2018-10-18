5d7cf9976f5c554c9bc5234ce430db27b5d0ca1a
Gonzalo Sáez Martí <Gonzalo Sez Mart@192.168.145.131>
2018-10-18 17:58:08 +0200

    Hecho el pop up modal.

--- aplicacion.php
+++ aplicacion.php
@@ -4,7 +4,8 @@
 ?>
 <div id="menu" style="color: white">
     <p></p>
-    <p><a class="btn btn-block btn-dark disabled">DEMUESTRA QUE ESTAS LISTO PARA LA EVAU</a></p>
+    <p><a onclick="muestraModalPrueba()" class="btn btn-block btn-dark">DEMUESTRA QUE ESTAS LISTO PARA LA EVAU</a></p>
+    <p><a class="btn btn-block btn-dark disabled"><?php echo $_SESSION['nombreUsuario'] ?></a></p>
     <p><a id="sigue1" class="btn btn-block btn-primary" onclick="sigue('1')">HISTORIA</a></p>
     <p><a id="sigue2" class="btn btn-block btn-primary" onclick="sigue('2')">ECONOMÍA</a></p>
     <p><a id="sigue3" class="btn btn-block btn-primary" onclick="sigue('3')">FILOSOFÍA</a></p>
@@ -23,5 +24,31 @@
             case '5': $("#menu").load("juego.php", {vidas:_vidas, correctas:0, tema:"Ingles"}); break;
         }
     }
+    
+    function muestraModalPrueba(){
+        $('#modalPrueba').modal('show');
+    }
 </script>
+
+
+<div id="modalPrueba" class="modal" tabindex="-1" role="dialog"  style="color: darkgray ">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal de prueba</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Hola usted está en un pop up de prueba.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Guardar</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php
